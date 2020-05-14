a6ca65611d7986c792833a50631923a3861f9fad
Loh Jian Rong <ipwnjianrong@gmail.com>
2020-05-14 22:28:39 +0800

    Added timer functionality. Stop and pause functionality has not been added.

--- scripts.js
+++ scripts.js
@@ -1,13 +1,61 @@
 const settings = document.querySelector('#settings');
 const gearBtn = document.querySelector('#gearBtn');
+const playBtn = document.querySelector("#play")
+const pauseBtn = document.querySelector("#pause")
 const pomodoroSlider = document.getElementById("pomodoro");
 const pomodoroOutput = document.getElementById("pomodoroCounter");
 const shortBreakSlider = document.getElementById("shortBreak");
 const shortBreakOutput = document.getElementById("shortBreakCounter");
 const longBreakSlider = document.getElementById("longBreak");
 const longBreakOutput = document.getElementById("longBreakCounter");
+const keys = document.querySelector("#multimediaButtons");
+const display = document.querySelector('#time');
 
 let settingsShown = false;
+
+function stopTimer() {
+    clearInterval(myTimer)
+}
+
+// Timer function
+function startTimer(duration, display) {
+    var start = Date.now(),
+        diff,
+        minutes,
+        seconds;
+    function timer() {
+        // get the number of seconds that have elapsed since 
+        // startTimer() was calledve elapsed since 
+        // startTimer() was called
+        diff = duration - (((Date.now() - start) 
+        diff = duration - (((Date.now() - start) / 1000) | 0);
+
+        // does the same job as parseInt truncates the float
+        minutes = (diff / 60) | 0;
+        seconds = (diff % 60) | 0;
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds; 
+
+        if (diff <= 0) {
+            // add one second so that the count down starts at the full duration
+            // example 05:00 not 04:59
+            start = Date.now() + 1000;
+        }
+    };
+    // we don't want to wait a full second before the timer starts
+    timer();
+    var myTimer = setInterval(timer, 1000);
+}
+
+playBtn.addEventListener("click", function () {
+    var fiveMinutes = +(60 * pomodoroSlider.value),
+        display = document.querySelector('#time');
+    startTimer(fiveMinutes, display);
+});
+pauseBtn.addEventListener("clicl", stopTimer);
 
 // Setting menu functions
 function showSettings() {
